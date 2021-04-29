eac3ad59c542c557b67247e8f523e416ff2f4fa9
刘雨阳 <liuyuyang.benjamin@bytedance.com>
2021-04-29 12:42:54 +0800

    fix: validation of number 0 always passes with `strict` option off

--- tests/number.test.js
+++ tests/number.test.js
@@ -8,34 +8,41 @@ it('number type', () => {
 it('number required', () => {
   expect(bv().number.isRequired.end.message).toBe('number is required');
   expect(bv(123).number.isRequired.end).toBe(null);
+  expect(bv(0).number.isRequired.end).toBe(null);
 });
 
 it('number.min', () => {
   expect(bv(-1).number.min(0).end.message).toBe('`-1` is not greater than `0`');
   expect(bv(2).number.min(1).end).toBe(null);
+  expect(bv(0).number.min(1).end.message).toBe('`0` is not greater than `1`');
 });
 
 it('number.max', () => {
   expect(bv(2).number.max(1).end.message).toBe('`2` is not less than `1`');
   expect(bv(2).number.max(3).end).toBe(null);
+  expect(bv(0).number.max(-1).end.message).toBe('`0` is not less than `-1`');
 });
 
 it('number.range', () => {
   expect(bv(2).number.range(3, 4).end.message).toBe('`2` is not in range `3 ~ 4`');
   expect(bv(2).number.range(1, 4).end).toBe(null);
+  expect(bv(0).number.range(3, 4).end.message).toBe('`0` is not in range `3 ~ 4`');
 });
 
 it('number.equal', () => {
   expect(bv(2).number.equal(2).end).toBe(null);
   expect(bv(2).number.equal(4).end.message).toBe('`2` is not equal to `4`');
+  expect(bv(0).number.equal(4).end.message).toBe('`0` is not equal to `4`');
 });
 
 it('number.positive', () => {
   expect(bv(2).number.positive.end).toBe(null);
   expect(bv(-1).number.positive.end.message).toBe('Expect number to be positive, but got `-1`');
+  expect(bv(0).number.positive.end.message).toBe('Expect number to be positive, but got `0`');
 });
 
 it('number.negative', () => {
   expect(bv(-1).number.negative.end).toBe(null);
   expect(bv(2).number.negative.end.message).toBe('Expect number to be negative, but got `2`');
+  expect(bv(0).number.negative.end.message).toBe('Expect number to be negative, but got `0`');
 });
